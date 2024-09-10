f8016b1a1bbf30970552b47f658076f42866993e
Emma <emmamwaura99@gmail.com>
2024-09-10 09:27:52 +0300

    modules added

---
+++ src/app/booking-system/booking-system.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BookingSystemRoutingModule } from './booking-system-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BookingSystemRoutingModule
+  ]
+})
+export class BookingSystemModule { }
---
+++ src/app/guest-management/guest-management.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GuestManagementRoutingModule } from './guest-management-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GuestManagementRoutingModule
+  ]
+})
+export class GuestManagementModule { }
